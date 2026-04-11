b369f0777a7dc79498e8297275a85b13c6d68bca
cc112FC <achulluncuyr@uni.edu.pe>
2026-04-11 08:03:36 -0500

    ejemplos if-else y switch

---
+++ semanas/semana03/ejercicios/ejemplo01.cpp
@@ -0,0 +1,29 @@
+﻿// Escribir un algoritmo que determine si un número es positivo negativo o cero
+
+#include <iostream>  // para usar std::cout <<  std::cin >>
+using namespace std;
+
+
+int main() {
+    // Datos
+	int n;
+	cout << "Ingrese un numero: " ;
+	cin >> n;
+
+    // Procesar , salida
+
+    if (n > 0) {
+        cout << "El numero es positivo" << endl;
+    } else  {   //  aquí n es negativo o cero
+        if (n < 0) {
+            cout << "El numero es negativo" << endl;
+        } else { // aquí n es cero
+            cout << "El numero es cero" << endl;
+        }
+    }
+		
+
+    return 0; // indica que el programa funciono correctamente
+}
+	
+	
---
+++ semanas/semana03/ejercicios/ejemplo02.cpp
@@ -0,0 +1,42 @@
+﻿// Escribir un algoritmo que pida ingresar un numero entre 1 y 7 y muestre el dia de la semana correspondiente
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	// Datos
+	int n; // Definir n Como Entero;
+	
+	cout << "Ingrese un entero entre 1 - 7: ";
+	cin >> n;
+	
+	
+	// Procesar datos y mostrar
+	switch (n) {
+		case 1:
+			cout << "Lunes\n";
+            break;
+		case 2:
+			cout << "Martes\n";
+            break;
+		case 3:
+			cout << "Miércoles\n";
+            break;
+		case 4:
+			cout << "Jueves\n";
+            break;
+		case 5:
+			cout << "Viernes\n";
+            break;
+		case 6:
+			cout << "Sábado\n";
+            break;
+		case 7:
+			cout << "Domingo\n";
+            break;
+		default:
+			cout << "El numero ingresado es incorrecto, debe estar entre 1 y 7\n";
+    }
+	
+}
