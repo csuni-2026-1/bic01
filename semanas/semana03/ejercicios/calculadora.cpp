83fab455a425a56ef0b5d7682e5c80c6a1bde7b7
cc112FC <achulluncuyr@uni.edu.pe>
2026-04-11 18:20:18 -0500

    agrega ejemplos puedeVotar calculadora basica

---
+++ semanas/semana03/ejercicios/calculadora.cpp
@@ -0,0 +1,50 @@
+﻿// Implementar una calculadora que dados 2 numeros, realice las 4 operaciones básicas (+, -. * y /)
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	// datos
+	double n1, n2, resultado;
+	int opcion;
+	
+	cout << "Ingrese 2 numeros: ";
+	cin >> n1 >> n2;
+	
+	cout << "seleccione una operacion: " << endl;
+	cout << "1: Suma" << endl;
+	cout << "2: Resta" << endl;
+	cout << "3: Multiplicacion" << endl;
+	cout << "4: Division" << endl;
+	cin >> opcion;
+
+    switch (opcion) {
+        case 1:
+            resultado = n1 + n2;
+			cout << "El resultado es: " << resultado << endl;
+            break;
+        case 2:
+            resultado = n1 - n2;
+			cout << "El resultado es: " << resultado << endl;
+            break;
+        case 3:
+            resultado = n1 * n2;
+			cout << "El resultado es: " << resultado <<endl;
+            break;
+        case 4:
+            if (n2 != 0) {
+				resultado = n1 / n2;
+				cout << "El resultado es: " << resultado <<endl;
+            } else {
+				cout << "Error ! Division por cero" <<endl;
+            }
+            break;
+        default:
+            cout << "Opcion invalida" << endl;
+            break;
+    }
+	
+
+    return 0;
+}
---
+++ semanas/semana03/ejercicios/puedeVotar.cpp
@@ -0,0 +1,33 @@
+﻿// Implementar un algoritmo que determine (decidir) si una persona puede votar
+
+#include <iostream> // para usar std::cout << , std::cin >>, std::endl;
+using namespace std; // para usar cout << , cin >>,  endl;
+
+int main() {
+    
+    // Datos
+	int edad; // Definir edad Como Entero
+    int tieneDNI; // Definir tieneDNI Como Entero // 1: si, 2: No
+	
+    cout << "Ingrese su edad: ";
+	cin >> edad;
+	cout << "¿Tiene DNI? (1: Si, 0: No): ";
+	cin >> tieneDNI;
+	
+    // Procesar
+	// Salida
+
+    if (edad>=18 && tieneDNI==1 ) {
+        cout << "Puede votar" <<endl; 
+    } else {
+        cout << "No puede votar" << endl;
+    }
+
+    return 0; // significa que su programa no tiene fallas
+
+
+}
+
+
+
+	
