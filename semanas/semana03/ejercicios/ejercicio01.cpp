304c0ee234e3a47551c1b257f04da3412555116a
cc112FC <achulluncuyr@uni.edu.pe>
2026-04-11 08:39:11 -0500

    agrega ejercicios 1 y 5 resueltos

---
+++ semanas/semana03/ejercicios/ejercicio01.cpp
@@ -0,0 +1,31 @@
+﻿/*
+Escribe un programa que pida una temperatura en grados Celsius (número decimal).
+Usa una cadena de if-else if para mostrar:
+    Si es menor a 0 °C → "Temperatura muy baja - Riesgo de congelación"
+    Si está entre 0 y 25 °C → "Temperatura normal"
+    Si está entre 25 y 40 °C → "Temperatura alta - Verificar equipo"
+    Si es mayor a 40 °C → "¡Peligro! Temperatura crítica
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Datos
+    double temperatura;
+
+    cout << "Ingrese la temperatura en grados Celsius: ";
+    cin >> temperatura;
+
+    if (temperatura < 0) {
+        cout << "Temperatura muy baja - Riesgo de congelación\n";
+    } else if ( temperatura < 25){     // 0 <= temperatura < 25
+        cout << "Temperatura normal\n";
+    } else if (temperatura < 40){     //25 <= temperatura < 40
+        cout << "Temperatura alta - Verificar equipo\n";
+    } else {
+        cout << "¡Peligro! Temperatura crítica\n";
+    }
+    return 0;
+}
---
+++ semanas/semana03/ejercicios/ejercicio05.cpp
@@ -0,0 +1,56 @@
+﻿/*
+Un administrador tiene un presupuesto de 5000 soles.
+El programa pide:
+    El monto del gasto (decimal)
+    El tipo de gasto (entero): 1 = Compra de materiales, 2 = Servicios, 3 = Sueldos, 4 =
+Otros
+Usa switch para mostrar un mensaje específico según el tipo de gasto y luego un if para
+verificar si el gasto excede el presupuesto:
+    Si el total supera 5000 → "¡Presupuesto excedido!"
+    Si no → "Gasto registrado correctamente
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // Datos
+    float presupuesto = 5000;
+    float gasto;
+    int tipo;
+
+    cout << "Ingrese el monto del gasto: ";
+    cin  >> gasto;
+
+    cout << "Ingrese el tipo de gasto (entero): 1 = Compra de materiales, 2 = Servicios, 3 = Sueldos, 4 = Otros: ";
+    cin >> tipo;
+
+    // Procesar
+    switch (tipo) {
+        case 1:
+            cout << "Registrando  gastos en compra de materiales\n";
+            break;
+        case 2:
+            cout << "Registrando  gastos en servicios\n";
+            break;
+         case 3:
+            cout << "Registrando  sueldos\n";
+            break;
+        case 4:
+            cout << "Registrando otros gastos\n";
+            break;
+        default:
+            cout << "Gasto inválido\n";
+            break;
+    }
+
+    if (gasto > presupuesto) {
+        cout << "Presupuesto excedido\n";
+    }
+    else {
+        cout << "Gasto registrado correctamente\n";
+    }
+
+
+    return 0;
+}
