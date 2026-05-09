313663e4f97df756bac2e8766e5d20ee50cd062a
cc112FC <achulluncuyr@uni.edu.pe>
2026-05-09 18:42:19 -0500

    agrega semena07/ejercicios para el Parcial

---
+++ semanas/semana07/teoria/ejemplo1.cpp
@@ -0,0 +1,22 @@
+﻿/*
+Mostrar la tabla de factoriales
+desde 0 hasta 10
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    for(int n = 0; n <= 10; ++n) { // recorrer numeros desde 0 hasta 10
+        int factorial = 1;
+        for(int i = 1; i <= n; ++i){// Para cada n calculamos su factorial
+            factorial *=i; 
+        }
+        cout << n << "! = " << factorial << endl; 
+    }
+
+    
+
+    return 0;
+}
---
+++ semanas/semana07/teoria/ejemplo2.cpp
@@ -0,0 +1,36 @@
+﻿/*
+Imprimir
+n = 4
+
+   *    ---> 3 espacios y 1 *   
+  ***   ---> 2 espacios y 3 *
+ *****  ---> 1 espacio  y 5 *
+*******
+
+
+M[i][j]
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n  = 4;
+
+    for(int i=1; i <= n; ++i) { // recorre las filas
+        // Para cada fila imprimimos espacios y
+        // espacios
+        for(int j =1; j<= n - i; ++j) {
+            cout << " ";
+        }
+
+        // Asteriscos
+        for(int j=1; j <= 2*i - 1; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+
+    }
+
+    return 0;
+}
