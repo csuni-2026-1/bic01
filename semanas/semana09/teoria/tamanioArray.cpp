6886b4371ddfbc1648951b48ae5ee460d43ae577
cc112FC <achulluncuyr@uni.edu.pe>
2026-05-21 19:30:36 -0500

    forma correcta de trabajar con arreglos

--- semanas/semana09/teoria/tamanioArray.cpp
+++ semanas/semana09/teoria/tamanioArray.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <vector>
 using namespace std;
 
 int main(){
@@ -9,11 +10,22 @@ int main(){
     // cin >> n;
     // int incorrecto[n];
 
-
-
     // FORMA CORRECTA:
     const int TAM = 10;
     int correcto[TAM];
+
+
+    
+    // TAMBIÉN PODEMOS HACER
+    int notas[100]; // tamaño suficiente
+    
+    // Luego pedimos ingreasar el número de notas (máximo 100)
+    int n;
+    cout << "Ingrese el número de notas (maximo 100): ";
+
+    // y solo trabajamos con una porción (las primeras n posiciones) del arreglo notas
+
+  
 
     return 0;
 }
